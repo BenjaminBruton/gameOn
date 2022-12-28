0e864db1a675f6c1d27d63238de7455f6ef254bd
BenjaminBruton <bbruton@wgu.edu>
2022-12-28 13:34:26 -0600

    style changes

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const classColl = document.getElementsByClassName("category-item");
 
 for (let i = 0; i < classColl.length; i++) {
   classColl[i].addEventListener("click", function onClick(event) {
-    event.target.style.color = "#EB0018";
+    event.target.style.color = "#32009E";
   });
 }
